5731b76b57caf9601b1aba349f8559a182ec8912
Julia Neverova <99186296+JuliaNeverova702@users.noreply.github.com>
2026-03-31 17:28:45 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -10,6 +10,6 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CONFLUENCE_USER = os.getenv("CONFLUENCE_USER")
 CONFLUENCE_PASSWORD = os.getenv("CONFLUENCE_PASSWORD")
 
-JIRA_URL = "https://jira.eltc.ru/rest/api/2/search"
-DIFY_URL = "http://localhost/v1/workflows/run"
+JIRA_URL = ""
+DIFY_URL = ""
 TELEGRAM_API = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
